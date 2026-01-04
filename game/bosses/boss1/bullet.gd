90b9d72cc7a0e7d35b956143f0b3edc332f7bc8c
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-01-04 15:43:14 -0500

    player gets some actual healht and changeed the bullets

--- game/bosses/boss1/bullet.gd
+++ game/bosses/boss1/bullet.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var velocity
 var dmg
+
 func _process(delta):
 	position += velocity*delta
 
@@ -10,6 +11,7 @@ func _on_hitbox_body_entered(body):
 	var damageable = body.find_child("EnemyDamageable")
 	if damageable:
 		damageable.hit(dmg)
+		queue_free()
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
