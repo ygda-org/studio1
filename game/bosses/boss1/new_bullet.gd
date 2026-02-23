89151e80002899b27f775c808160c4c47c225c0f
CookieFinder23 <190116541+CookieFinder23@users.noreply.github.com>
2026-02-23 17:13:45 -0500

    z

---
+++ game/bosses/boss1/new_bullet.gd
@@ -0,0 +1,35 @@
+extends RigidBody2D
+
+var dmg
+var gravity_affected
+var velocity
+
+func _ready():
+	position = get_parent().get_node("Boss").position
+
+func _physics_process(delta):
+	if velocity:
+		if gravity_affected:
+			velocity.y += 4
+			var collision_info = move_and_collide(velocity * delta)
+			if collision_info:
+				if not collision_info.get_collider().find_child("EnemyDamageable"):
+						velocity = velocity.bounce(collision_info.get_normal())
+						velocity *= 0.7
+						if velocity.length() < 20:
+							queue_free()
+		else:
+			move_and_collide(velocity * delta)
+	else:
+		queue_free()
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	var damageable = body.find_child("EnemyDamageable")
+	if damageable:
+		damageable.hit(dmg)
+		queue_free()
+	if not gravity_affected:
+		queue_free()
--- game/bosses/boss1/projectiles.gd
+++ game/bosses/boss1/projectiles.gd
@@ -5,7 +5,14 @@ signal change_phase
 const RETURN_TO_CENTER_SPEED = 100
 const PROJECTILE_SPEED = 150
 const PROJECTILE_DMG = 10
+var attack
+var double
 @onready var projectile_position = states.get_parent().get_parent().get_node("ProjectilePosition").global_position
+
+enum Attacks {
+	BULLETS,
+	GRAVITY_BULLETS
+}
 
 func _ready():
 	get_parent().global_position = projectile_position
@@ -14,27 +21,39 @@ func _ready():
 func activation():
 	$PhaseTimer.wait_time = randf_range(5, 10)
 	$PhaseTimer.start()
+	$BulletCD.start()
+	attack = Attacks.values().pick_random()
 	
 func _on_phase_timer_timeout() -> void:
 	change_phase.emit()
 
-func _process(_delta):
-	if not active:
-		return
-	
-	if active and $BulletCD.is_stopped() and NetworkState.is_server():
-		$BulletCD.start()
-		var target = GameState.players.pick_random()
-		var velocity = get_parent().global_position.direction_to(target.global_position) * PROJECTILE_SPEED
-		shoot.rpc(target, velocity)
-		
-
 @rpc ("authority", "call_local")
-func shoot(target, velocity):
+func shoot(target, velocity, gravity_affected):
 	if not target:
 		return
 	var projectile = load("res://bosses/boss1/bullet.tscn").instantiate()
 	projectile.velocity = velocity
 	projectile.dmg = PROJECTILE_DMG
 	projectile.global_position = global_position
+	projectile.gravity_affected = gravity_affected
 	states.get_parent().get_parent().add_child(projectile)
+
+
+func _on_bullet_cd_timeout() -> void:
+	if not active:
+		return
+	
+	if active and NetworkState.is_server():
+		$BulletCD.start()
+		var target = GameState.players.pick_random()
+		double = abs(global_position.x) < 5
+		if attack == Attacks.GRAVITY_BULLETS:
+			var velocity = get_parent().global_position.direction_to(target.global_position) * PROJECTILE_SPEED + Vector2(randf_range(-20, 20), -100)
+			shoot.rpc(target, velocity, true)
+			if double:
+				shoot.rpc(target, Vector2(velocity.x * -1, velocity.y), true)
+		else:
+			var velocity = get_parent().global_position.direction_to(target.global_position) * PROJECTILE_SPEED + Vector2(randf_range(-10, 10), randf_range(-10, 10))
+			shoot.rpc(target, velocity, false)
+			if double:
+				shoot.rpc(target, Vector2(velocity.x * -1, velocity.y), false)
