53bc58944062dcc8f1287726f3c4ce15a8cf40b5
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-03-07 09:35:02 -0500

    quick reorganization

--- game/main/globals/network_state.gd
+++ game/main/globals/network_state.gd
@@ -14,7 +14,7 @@ var peer: ENetMultiplayerPeer
 
 var room_password: String = "placeholder_password"
 var players: Dictionary[int, PlayerInfo] = {} # ID -> Player info
-var this_player_role = null
+var this_player_role = "uid://c3yx6fq1qrhfd"
 
 func _ready() -> void:
 	multiplayer.peer_connected.connect(_player_joined)
